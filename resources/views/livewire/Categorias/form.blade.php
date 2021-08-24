9b754247bdcdc128c2ca5c6638f4e4a575b94560
WaynerEP <espinozaperez2504@gmail.com>
2021-08-24 00:49:07 -0500

    cambios en las categorias

---
+++ resources/views/livewire/Categorias/form.blade.php
@@ -0,0 +1,65 @@
+<!-- Modal -->
+<div wire:ignore.self class="modal fade" id="categoryModal" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{ $title ? 'Editar Categoria' : 'Nueva Categoria' }}
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="feather feather-x">
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group mb-4">
+                    <label for="nombre">Descripción*</label>
+                    <input type="text" wire:model.defer="nombre" class="form-control form-control-sm" id="nombre"
+                        placeholder="ej. Bebidas" required>
+                    @error('nombre')
+                        <div class="invalid-feedback d-block" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-sm" wire:click="resetUI" data-dismiss="modal"><i class="flaticon-cancel-12"></i>
+                    Cancelar</button>
+                @if ($title)
+                    <button type="button" wire:click="Update()" class="btn btn-info btn-sm flex align-items-center">
+                        <svg wire:loading wire:target="Update" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader spin mr-2">
+                            <line x1="12" y1="2" x2="12" y2="6"></line>
+                            <line x1="12" y1="18" x2="12" y2="22"></line>
+                            <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+                            <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+                            <line x1="2" y1="12" x2="6" y2="12"></line>
+                            <line x1="18" y1="12" x2="22" y2="12"></line>
+                            <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+                            <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+                        </svg> Actualizar</button>
+                @else
+                    <button type="button" wire:click="Store()" class="btn btn-primary btn-sm flex align-items-center">
+                        <svg wire:loading wire:target="Store" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader spin mr-2">
+                            <line x1="12" y1="2" x2="12" y2="6"></line>
+                            <line x1="12" y1="18" x2="12" y2="22"></line>
+                            <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+                            <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+                            <line x1="2" y1="12" x2="6" y2="12"></line>
+                            <line x1="18" y1="12" x2="22" y2="12"></line>
+                            <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+                            <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+                        </svg> Guardar</button>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
